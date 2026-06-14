6109945c8db565d865590cc57be9d55796c60266
pona <nataliaponce60@gmail.com>
2026-06-14 10:56:01 -0300

    Add section tasaciones

--- inmobiliaria/back/routes/admin/login.js
+++ inmobiliaria/back/routes/admin/login.js
@@ -18,6 +18,7 @@ router.post('/', async(req, res, next) => {
         if (data != undefined) {
             req.session.id_usuario = data.id;
             req.session.nombre = data.usuario;
+            res.redirect('/admin/tasaciones');
         } else {
             res.render('admin/login', {layout: 'admin/layout', error: true});
         }
