f9b275af43ce98a8c9b49c037a0c199bb98d877e
pona <nataliaponce60@gmail.com>
2026-06-15 09:50:32 -0300

    Add navigation component with links to Home, Nosotros, Tasaciones, and Contacto

---
+++ inmobiliaria/front/src/components/Nav.js
@@ -0,0 +1,19 @@
+ 'use client'
+ import Link from "next/link";
+ import { usePathname } from "next/navigation";
+
+ export default function Nav() {
+    const pathName = usePathname();
+    const isActive = (path) => path === pathName;
+
+    return (
+        <nav>
+            <ul className="holder">
+                <li><Link className={isActive("/") ? 'activo' : ''} href="/">Home</Link></li>
+                <li><Link className={isActive("/nosotros") ? 'activo' : ''} href="/nosotros">Nosotros</Link></li>
+                <li><Link className={isActive("/tasaciones") ? 'activo' : ''} href="/tasaciones">Tasaciones</Link></li>
+                <li><Link className={isActive("/contacto") ? 'activo' : ''} href="/contacto">Contacto</Link></li>
+            </ul>
+        </nav>
+    )
+ }
